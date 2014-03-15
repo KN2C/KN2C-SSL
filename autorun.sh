292fd326150c15338282fc3744ec770464feb7f0
Faraz Fallahi <fffaraz@gmail.com>
2014-03-15 15:17:02 +0330

    restart

--- autorun.sh
+++ autorun.sh
@@ -7,13 +7,13 @@ cd bin
 
 while true; do
 
+  echo "Restarting kn2cssl ..."
   pkill -f "kn2cssl"
   ./kn2cssl nogui &
 
   OLD_MD5=$(md5sum kn2cssl)
   NEW_MD5=$OLD_MD5
   while [ "$OLD_MD5" == "$NEW_MD5" ]; do
-    echo `date`
     sleep 15
     NEW_MD5=$(md5sum kn2cssl)
   done
